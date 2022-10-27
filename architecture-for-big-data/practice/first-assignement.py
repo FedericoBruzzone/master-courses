faf6f8d4ff24287322d1fee976afa7f3a051e02c
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2022-10-27 17:50:45 +0200

    Update `alg` and `arch`
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- architecture-for-big-data/practice/first-assignement.py
+++ architecture-for-big-data/practice/first-assignement.py
@@ -33,90 +33,99 @@ class AbstractDatabase(ABC):
         self.database = database
 
     @abstractmethod
-    def execute_query(self, sql, parameters={}): pass
+    def execute_query(self, sql): pass
 
 class IDatabaseSQL(AbstractDatabase):
-    connection: str = ''
-    curr: str = ''
+    _connection: str = ''
+    _curr: str = ''
 
     def __init__(self, host: str, database: str, user: str = '', password: str = '', port: str = ''):
-        self.connection = self._database_connection(user, password, port, host, database)
-        # self.curr = self.connection.cursor()
+        self._connection = self._database_connection(user, password, port, host, database)
+        # self._curr = self._connection.cursor()
         super().__init__(host, database, user, password, port)
 
     @abstractmethod
     def _database_connection(self, user: str, password: str, port: str, host: str, database: str): pass
 
-    def execute_query(self, sql, parameters={}):
-        # parameters issue
+    def execute_query(self, sql): 
         
-        #return self.curr.execute(sql)
+        # 'INSERT INTO table_name (column1, column2, column3, ...) VALUES (value1, value2, value3, ...)' 
+        
+        # return self.curr.execute(sql)
         print('Query has been executed', sql)
 
-class Service(IDatabaseSQL): # IMPORANT: e.g., for MySQL
+class Service(IDatabaseSQL): 
     # import mysql.connector
 
     def _database_connection(self, host: str, database: str, user: str, password: str, port: str):
-        # connection = mysql.connector.connect( 
+        # self._connection = mysql.connector.connect( 
         #     host=host, 
         #     user=user, 
         #     password=password, 
         #     database=database 
         # )
-        # return connection
-        print('Succesfull connection', host, database, user, password, port)
+        # return self.connection != ''
+        print('Succesfull connection to the service (CLOUD)', host, database, user, password, port)
 
-# class ClientInterface(ABC):
-#     @abstractmethod
-#     def execute(self, sql): pass
+class ClientInterface(ABC):
+    @abstractmethod
+    def _execute(self, sql): pass
 
-#     @abstractmethod
-#     def make_readable(): pass
+    @abstractmethod
+    def get_data(self, sql): pass
 
-# class ServiceAdapter(ClientInterface):
-#     service: Service = Service()
+class ServiceAdapter(ClientInterface):
+    service: Service = ''
 
-#     def __init__(self, service: Service):
-#         self.service = service
+    def __init__(self, service: Service):
+        self.service = service
 
-#     @override
-#     def execute(sql):
-#         return service.execute_query(sql)
+    def _execute(self, sql):
+        # return self.service.execute_query(sql)
+        print('Adapter: Query has been executed', sql)
+        return True
 
-#     @override
-#     def make_readable(sql):
-#         pass # IMPLEMENTED
+    def get_data(self, sql):
+        res = self._execute(sql) 
+        # return e.g., JSON fromat from query sorted by column
+        return [('value1.1', 'value2.1'), ('value1.2', 'value2.2'), ('value1.3', 'value2.3')]
 
-# class Client(IDatabaseSQL): # IMPORANT: e.g., for MariaDB
-#     import mariadb
+class Client(IDatabaseSQL): 
+    # import mariadb
 
-#     clientInterface: ClientInterface = ClientInterface()
+    clientInterface: ClientInterface = ''
 
-#     def __init__(self, clientInterface: ClientInterface):
-#         self.clientInterface = clientInterface
-#         super().__init__()
+    def __init__(self, clientInterface: ClientInterface, host: str, database: str, user: str = '', password: str = '', port: str = ''):
+        self.clientInterface = clientInterface
+        super().__init__(host, database, user, password, port)
 
-#     @override
-#     def _database_connection(self, ser: str, password: str, port:str, host: str, database: str):
-#         return connection = mariadb.connect(
-#             user=user, #"db_user"
-#             password=password, #"db_user_passwd"
-#             host=host, #"192.0.2.1"
-#             port=port, #3306
-#             database=database #"employees"
-#         )
+    def _database_connection(self, user: str, password: str, port:str, host: str, database: str):
+        # self._connection = mariadb.connect(
+        #     user=user,
+        #     password=password, 
+        #     host=host, 
+        #     port=port, 
+        #     database=database
+        # )
+        # return self.connection != ''
+        print('Succesfull connection to the client (LOCAL)', host, database, user, password, port)
 
-#     def writeUser(cloud_database_sql, local_database_query):
-#         users = make_readable(clientInterface.execute_query(cloud_database_sql))
+    def write(self, cloud_query, table, column=''):
+        # local_table is in local_database
 
-#         local_write = self.connection.execute_query(local_database_query, users)
+        resp = self.clientInterface.get_data(cloud_query) # Response is readable
+
+        for i in resp:
+            self.execute_query(f'INSERT INTO {table} {column} VALUES {i};')
+
+        return True
 
 if __name__ == '__main__':
     cloud_database: Service = Service('cloud_database.com', 'mysql_cloud_database')
-    cloud_database.execute_query('SELECT * FROM user')
+    # cloud_database.execute_query('SELECT * FROM user')
 
-    # adapter_cloud_database = ServiceAdapter(cloud_database)
-
-    # local_database: Client = Client(adapter_cloud_database, 'user', 'password', 'port', 'host', 'database')
-
-    # local_database.writeUser({'SELECT * FROM user', 'INSERT user'})
+    adapter_cloud_database: ServiceAdapter = ServiceAdapter(cloud_database)
+    # adapter_cloud_database.execute('SELECT * FROM user')
+    
+    local_database: Client = Client(adapter_cloud_database, 'localhost.com', 'mariadb_local_database')
+    local_database.write('SELECT * FROM user', 'user', ('value1', 'value2'))
