0d799121e2ef23fc902d2d576e13ce50b02e6103
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2022-11-01 15:52:20 +0100

    Update `first-assignement` (it works)
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- architecture-for-big-data/practice/first-assignement/first-assignement.py
+++ architecture-for-big-data/practice/first-assignement/first-assignement.py
@@ -17,6 +17,7 @@
 # 6. (Avoiding handover and people lock-in)
 
 from abc import ABC, abstractmethod
+import mysql.connector
 
 class AbstractDatabase(ABC):
     user: str = ''
@@ -25,7 +26,7 @@ class AbstractDatabase(ABC):
     host: str = ''
     database: str  = ''
 
-    def __init__(self, host: str, database: str, user: str = '', password: str = '', port: str = ''):
+    def __init__(self, user: str, password: str, host: str, database: str, port: str = ''):
         self.host = host
         self.database = database
         self.user = user
@@ -39,92 +40,83 @@ class IDatabaseSQL(AbstractDatabase):
     _connection: str = ''
     _curr: str = ''
 
-    def __init__(self, host: str, database: str, user: str = '', password: str = '', port: str = ''):
-        self._connection = self._database_connection(user, password, port, host, database)
-        # self._curr = self._connection.cursor()
-        super().__init__(host, database, user, password, port)
+    def __init__(self, user: str, password: str, host: str, database: str, port: str = ''):
+        self._connection = self._database_connection(user, password, host, database, port)
+        self._curr = self._connection.cursor()
+        super().__init__(user, password, host, database, port)
 
     @abstractmethod
-    def _database_connection(self, host: str, database: str, user: str = '', password: str = '', port: str = ''): pass
+    def _database_connection(self, user: str, password: str, host: str, database: str, port: str = ''): pass
 
     def execute_query(self, sql): 
-        
-        # 'INSERT INTO table_name (column1, column2, column3, ...) VALUES (value1, value2, value3, ...)' 
-        
-        # return self.curr.execute(sql)
-        print('Query has been executed', sql)
+        self._curr.execute(sql)
+        res = [i for i in self._curr]
 
+        print(f'\nQuery has been executed: {sql}')
+        for i in res:
+            print(f"{i}")
+       
+        return res
+        
 class Service(IDatabaseSQL): 
-    # import mysql.connector
-
-    def _database_connection(self, host: str, database: str, user: str = '', password: str = '', port: str = ''):
-        # self._connection = mysql.connector.connect( 
-        #     host=host, 
-        #     user=user, 
-        #     password=password, 
-        #     database=database 
-        # )
-        # return self.connection != ''
-        print('Succesfull connection to the service (CLOUD)', host, database, user, password, port)
-
+    def _database_connection(self, user: str, password: str, host: str, database: str, port: str = ''):
+        self._connection = mysql.connector.connect(user=user, password=password, host=host, database=database)
+        print(f'Connection successfull to the: {host} {database} {user} {password} {port}')
+        return self._connection
+        
 class ClientInterface(ABC):
     @abstractmethod
     def _execute(self, sql): pass
 
-    def get_data(self, sql): pass
+    @abstractmethod
+    def read_from(self, sql): pass
 
 class ServiceAdapter(ClientInterface):
     service: Service = ''
 
     def __init__(self, service: Service):
         self.service = service
+        print('ServiceAdapter has been created')
 
     def _execute(self, sql):
-        # return self.service.execute_query(sql)
-        print('Adapter: Query has been executed', sql)
-        return True
-
-    def get_data(self, sql):
-        res = self._execute(sql) 
-        # return e.g., JSON fromat from query sorted by column
-        return [('value1.1', 'value2.1'), ('value1.2', 'value2.2'), ('value1.3', 'value2.3')]
+        res = self.service.execute_query(sql)
+        return res
+    
+    def read_from(self, sql):
+        query_res = self._execute(sql) 
+        return query_res
 
 class Client(IDatabaseSQL): 
-    # import mariadb
-
     clientInterface: ClientInterface = ''
 
-    def __init__(self, clientInterface: ClientInterface, host: str, database: str, user: str = '', password: str = '', port: str = ''):
+    def __init__(self, clientInterface: ClientInterface, user: str, password: str, host: str, database: str, port: str = ''):
         self.clientInterface = clientInterface
-        super().__init__(host, database, user, password, port)
+        super().__init__(user, password, host, database, port)
 
-    def _database_connection(self, user: str, password: str, port:str, host: str, database: str):
-        # self._connection = mariadb.connect(
-        #     user=user,
-        #     password=password, 
-        #     host=host, 
-        #     port=port, 
-        #     database=database
-        # )
-        # return self.connection != ''
-        print('Succesfull connection to the client (LOCAL)', host, database, user, password, port)
+    def _database_connection(self, user: str, password: str, host: str, database: str, port: str = ''):
+        self._connection = mysql.connector.connect(user=user, password=password, host=host, database=database)
+        print(f'Connection successfull to the: {host} {database} {user} {password} {port}')
+        return self._connection
 
-    def write(self, cloud_query, table, column=''):
-        # local_table is in local_database
-
-        resp = self.clientInterface.get_data(cloud_query) # Response is readable
-
-        for i in resp:
-            self.execute_query(f'INSERT INTO {table} {column} VALUES {i};')
-
+    def write_to(self, cloud_query, table, column=''):
+        response = self.clientInterface.read_from(cloud_query) 
+        new_column = f"({', '.join(column)})"
+        for i in response:
+            #print(f'INSERT INTO {table} {(new_column)} VALUES {(name, surname)};')
+            self.execute_query(f'INSERT INTO {table} {new_column} VALUES {(i)};')  
+        self._connection.commit()
         return True
 
 if __name__ == '__main__':
-    cloud_database: Service = Service('cloud_database.com', 'mysql_cloud_database')
+    cloud_database: Service = Service(user='root', password='welcome123', host='127.0.0.1', database='test_database')
     # cloud_database.execute_query('SELECT * FROM user')
 
     adapter_cloud_database: ServiceAdapter = ServiceAdapter(cloud_database)
-    # adapter_cloud_database.execute('SELECT * FROM user')
+    #adapter_cloud_database._execute('SELECT * FROM user')
+    #adapter_cloud_database.read_from('SELECT * FROM user')
+
+    local_database: Client = Client(adapter_cloud_database, user='root', password='welcome123', host='127.0.0.1', database='test_database')
+    #local_database.execute_query('SELECT * FROM user')
+    local_database.write_to('SELECT name, surname FROM user', 'user2', ('name', 'surname'))
+
     
-    local_database: Client = Client(adapter_cloud_database, 'localhost.com', 'mariadb_local_database')
-    local_database.write('SELECT * FROM user', 'user', ('value1', 'value2'))
