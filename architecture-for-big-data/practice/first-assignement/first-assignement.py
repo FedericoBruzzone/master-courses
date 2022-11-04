d841f6cc6ff66bc4a1da9e48000ea46d9dbe077b
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2022-11-04 17:44:31 +0100

    Update `first-ass` -> `CDC` and name class
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- architecture-for-big-data/practice/first-assignement/first-assignement.py
+++ architecture-for-big-data/practice/first-assignement/first-assignement.py
@@ -18,9 +18,11 @@
 
 from abc import ABC, abstractmethod
 import mysql.connector
+import json
 
 '''
-@dev AbstractDatabase is an abstract class that contains the information to connect with any database
+@dev AbstractDatabase is an abstract class that contains the information to connect with any database.
+     In the inherit class, you will have to implement `execute_query(...)` method.
 '''
 class AbstractDatabase(ABC):
     _user: str = ''
@@ -40,23 +42,30 @@ class AbstractDatabase(ABC):
     def execute_query(self, sql): pass
 
 '''
-@dev IDatabaseSQL are an abstract classes that define behavior of the specified database
+@dev IDatabaseSQL are an abstract class that define behavior of the SQL database.
+     Since IDatabaseSQL inherits from AbstractDatabase it must implements `execute_query(...)` method.
+     In the inherit class, you will have to implement `_database_connection()` method.
+
+     Assuming that in python any database sql library implement `.connector.connect(...)` and `.cursor()` methods: 
+        1. we use `_connection` field to store the connection to the specific database;
+        2. we use `_cursor` field to store the cursor to the specific database.
+        Note that `_cursor` contanis the `.execute()` method that is used to execute the query.
 '''
 class IDatabaseSQL(AbstractDatabase):
     _connection: str = ''
-    _currsor: str = ''
+    _cursor: str = ''
 
     def __init__(self, user: str, password: str, host: str, database: str, port: str = ''):
         self._connection = self._database_connection(user, password, host, database, port)
-        self._currsor = self._connection.cursor()
+        self._cursor = self._connection.cursor()
         super().__init__(user, password, host, database, port)
 
     @abstractmethod
     def _database_connection(self, user: str, password: str, host: str, database: str, port: str = ''): pass
     
     def execute_query(self, sql): 
-        self._currsor.execute(sql)
-        res = [i for i in self._currsor]
+        self._cursor.execute(sql)
+        res = [i for i in self._cursor]
 
         print(f'\nQuery has been executed: {sql}')
         for i in res:
@@ -65,7 +74,8 @@ class IDatabaseSQL(AbstractDatabase):
         return res
 
 '''
-@dev ExternalDatabase is a concrete class that contains the methods to read from the external database
+@dev ExternalDatabase is a concrete class that allow us to establish a connection.
+     Since ExternalDatabase inherits from IDatabaseSQL it must implements `_database_connection(...)`.
 '''
 class ExternalDatabase(IDatabaseSQL): 
     def _database_connection(self, user: str, password: str, host: str, database: str, port: str = ''):
@@ -74,7 +84,8 @@ class ExternalDatabase(IDatabaseSQL):
         return self._connection
 
 '''
-@dev IHistoricalDatabse is an interface that contains the delcarations of methods defined for each adapters
+@dev IHistoricalDatabse is an interface that contains the delcarations of methods that they must be implemented by each adapters.
+     We expect the `read_from(...)` mothod returns a fitted content for the HistoricalDatabase. 
 '''
 class IHistoricalDatabse(ABC):
     @abstractmethod
@@ -84,19 +95,20 @@ class IHistoricalDatabse(ABC):
     def read_from(self, sql): pass
 
 '''
-@dev ExternalDatabaseAdapter is a concrete class that inherits from IHistoricalDatabse and contains a ExternalDatabase object. 
-     Its purpose is to get data from service and make it readable for the client 
-     through the implemented IHistoricalDatabse methods.
+@dev ExternalDatabaseAdapter is a concrete class that make the data from ExternalDatadase readable and writable for HistoricalDatabase. 
+     Since ExternalDatabaseAdapter inherits from IHistoricalDatabse it must implements `_read_from(...)` and `_execute(...) methods`.
+     Since ExternalDatabaseAdapter has a ExternalDatabase object and it can execute query or more specifically read the data from it.
+     (e.g., If the our HistoricalDatabase would like to has a list of tuple we should implement this method in the way it returns it)
 '''
 class ExternalDatabaseAdapter(IHistoricalDatabse):
-    service: ExternalDatabase = ''
+    external_databse: ExternalDatabase = ''
 
-    def __init__(self, service: ExternalDatabase):
-        self.service = service
+    def __init__(self, external_databse: ExternalDatabase):
+        self.external_databse = external_databse
         print('ExternalDatabaseAdapter has been created')
 
     def _execute(self, sql):
-        query_res = self.service.execute_query(sql)
+        query_res = self.external_databse.execute_query(sql)
         return query_res
     
     def read_from(self, sql):
@@ -104,14 +116,28 @@ class ExternalDatabaseAdapter(IHistoricalDatabse):
         return query_res
 
 '''
-@dev HistoricalDatabse is a concrete class that contains the methods to write in the historical database,
-     to make it, it use the IHistoricalDatabse methods to read adapted data from the external database.
+@dev HistoricalDatabse is a concrete class that contains the methods to write into the historical database,
+     Since HistoricalDatabse inherits from IDatabaseSQL it must implements `_database_connection(...)`.
+     Since HistoricalDatabse has a IHistoricalDatabase object it can get data from it.
+     
+     The `.write_to(...)` mothod use the `.read_from(...)` of the adapter to get a list of tuple 
+     and then it will insert it to historical database. 
+     After this, it will commit the changes.
 '''
 class HistoricalDatabse(IDatabaseSQL): 
-    clientInterface: IHistoricalDatabse = ''
+    adapter_for_historical_database: IHistoricalDatabse = ''
+    last_sync: int = 0 
 
-    def __init__(self, clientInterface: IHistoricalDatabse, user: str, password: str, host: str, database: str, port: str = ''):
-        self.clientInterface = clientInterface
+    def __init__(self, adapter_for_historical_database: IHistoricalDatabse, user: str, password: str, host: str, database: str, port: str = ''):
+        self.adapter_for_historical_database = adapter_for_historical_database
+        try:
+            #self.last_sync = self.adapter_for_historical_database.read_from('SELECT id FROM user ORDER BY id DESC LIMIT 1')[0][0]
+            with open("sync.json", "r") as json_object:
+                sync_data = json.load(json_object)
+                self.last_sync = sync_data['sync_last']
+        except:
+            with open('sync.json', 'w'): pass
+            self.last_sync = 0
         super().__init__(user, password, host, database, port)
 
     def _database_connection(self, user: str, password: str, host: str, database: str, port: str = ''):
@@ -119,19 +145,23 @@ class HistoricalDatabse(IDatabaseSQL):
         print(f'Connection successfull to the: {host} {database} {user} {password} {port}')
         return self._connection
 
-    def write_to(self, read_query, table, column='') -> bool:
-        response = self.clientInterface.read_from(read_query)
+    def write_to(self, read_query, table, column=''):
+        response = self.adapter_for_historical_database.read_from(read_query)
         new_column = f"({', '.join(column)})"
         for i in response:
             self.execute_query(f'INSERT INTO {table} {new_column} VALUES {(i)};') 
-
         self._connection.commit()
+
+        dictionary = {'sync_last': \
+            self.adapter_for_historical_database.read_from('SELECT id FROM user ORDER BY id DESC LIMIT 1')[0][0]}
+        json_object = json.dumps(dictionary)
+        with open("sync.json", "w") as sync_file:
+            sync_file.write(json_object)    
+
         return True
 
 if __name__ == '__main__':
     external_database: ExternalDatabase = ExternalDatabase(user='root', password='welcome123', host='127.0.0.1', database='test_database')
     adapter_external_database: ExternalDatabaseAdapter = ExternalDatabaseAdapter(external_database)
     historical_database: HistoricalDatabse = HistoricalDatabse(adapter_external_database, user='root', password='welcome123', host='127.0.0.1', database='test_database')
-    historical_database.write_to('SELECT name, surname FROM user', 'user2', ('name', 'surname'))
-
-    
+    historical_database.write_to(f'SELECT name, surname FROM user where id > {historical_database.last_sync}', 'user2', ('name', 'surname'))
