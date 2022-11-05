f9739f8a447c455b20d2d99c64c4a83fd0c9e03d
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2022-11-05 15:04:01 +0100

    Update `gruenbergers` file
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- advanced-programming/practice/exams/2010-02-22/gruenbergers-prime-path.py
+++ advanced-programming/practice/exams/2010-02-22/gruenbergers-prime-path.py
@@ -4,7 +4,7 @@ def is_prime(n):
 def gruenbergersPrimePath():
     def gen_odd_numers():
         i = 5
-        while i < 10:
+        while i < 10001:
             yield i
             i += 2
 
@@ -16,25 +16,39 @@ def gruenbergersPrimePath():
                ((is_prime(n) and (n+1) % 6 == 0) and -1) or \
                                                       0
 
-    def intersection(list):
-        pass
+    def generate_path():
+        g = gen_odd_numers()
+        update = 1 # x = 1, y = 0
+        last_move = 0
+        l = [[0,0], [0,1]]
 
+        for i in g:
+            if choice(i) != 0:
+                last_move = choice(i)
+                update = not update
+
+            x = l[-1].copy()
+            x[update] += last_move
+            l.append(x)
+
+        return l
     
-    g = gen_odd_numers()
-    update = 0 # x = 1, y = 0
-    last_move = 0
-    l = [[0,0], [0,1]]
+    def intersection():
+        path = generate_path()
+        memo = []
+        res = []
+        def inner():
+            for i in path:
+                if i not in memo:
+                    memo.append(i)
+                else:
+                    res.append(i)
+            print(all([True if path.count(i) > 1 else False for i in res]))
+            return res
 
-    for i in g:
-        if choice(i) != 0:
-            last_move = choice(i)
-        else:
-            update = not update
+        return inner()
 
-        x = l[-1]
-        x[update] += last_move
-        l.append(x)
-
-    return l
-
+    return intersection()
+    
 print(gruenbergersPrimePath())
+#gruenbergersPrimePath()
