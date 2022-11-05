c5f3b8dd3158d08f714fa3e7d42e132f4ca9ff9d
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2022-11-05 17:47:23 +0100

    Add `4`
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

---
+++ advanced-programming/practice/exams/2010-02-22/4-test-driven-development-of-prime-factors.py
@@ -0,0 +1,6 @@
+'''
+Exercise 4: Test Driven Development of Prime Factors.
+Prime Factors. There are two kinds of positive numbers: prime numbers and composite numbers. A composite number is the product of a sequence of prime numbers. You can write a simple generator, named prime_factors to factor numbers and yield each prime factor of the number; the generators take as an input the number to factor and returns the prime factors from the smallest to the largest. Note that, a simple-looking for -loop shall not work; the prime factor of 128 is 2, repeated 7 times. Note also that 1 is not a factor or better is always a factor so can be excluded by the list.
+
+The generator have to be developed by using the test-driven techniques; a commitment without test cases will be considered completely wrong.
+'''
