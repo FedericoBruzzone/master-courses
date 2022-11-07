5d6bfcf9fd57ff4acecf32273839573f074f3a41
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2022-11-07 13:00:26 +0100

    Update `apt`
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- advanced-programming-techniques/practice/test/java_reflection/java-lang-reflect-method.java
+++ advanced-programming-techniques/practice/test/java_reflection/java-lang-reflect-method.java
@@ -4,13 +4,23 @@ import java.lang.reflect.Method;
 import java.util.stream.*;
 import java.util.Arrays;
 
-class MOP {  // meta-object protocol
+class MOP_java_lang__reflect_method {  // meta-object protocol
     public static String formalParametersToString(Method m) {
-        var mt = Arrays.asList(m.getParameterTypes());
-        return IntStream.range(0, m.getParameterCount()).boxed().map(i -> { return MOP_java_lang_class.classNameToString(mt.get(i))+" p"+(i+1);}).collect(Collectors.joining(", "));
+        var method = Arrays.asList(m.getParameterTypes());
+        
+        return IntStream.range(0, m.getParameterCount())
+                        .boxed()
+                        .map(i -> { return MOP_java_lang_class.classNameToString(method.get(i))+" p"+(i+1);})
+                        .collect(Collectors.joining(", "));
     }
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class<?> new_class = Class.forName("java.util.stream.IntStream");
+         
+        Stream<Method> methods  = Arrays.asList(new_class.getDeclaredMethods())
+                                        .stream()
+                                        .filter(s -> s.getName() == "iterate");
+        methods.forEach(method -> 
+            System.out.println(method.getName()+": "+formalParametersToString(method)));
     }
 }
