2683a8c2ed4150f1be92a135944bd859a76fb339
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2022-11-10 17:27:12 +0100

    Add `java` files
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- advanced-programming-techniques/practice/java_reflection/class_loader/1-a-broken-singleton.java
+++ advanced-programming-techniques/practice/java_reflection/class_loader/1-a-broken-singleton.java
@@ -25,7 +25,7 @@ class SimpleClassLoader extends ClassLoader {
     }
 
     protected byte[] getClassData(String directory, String fileName) {
-        String classFile = directory + "/" + fileName.replace(".", "/") + ".class";
+        String classFile = directory + "/" + fileName.replace('.', '/') + ".class";
         int classSize = (int) (new File(classFile)).length();
         byte[] buf = new byte[classSize];
         try {
@@ -53,14 +53,18 @@ class Singleton {
 
 class SingletonViolationTest {
     public static void main(String[] args) throws Exception {
+        String s = new String();
+        System.out.println(s.getClass().getClassLoader());
         SimpleClassLoader CL1 = new SimpleClassLoader("testclasses");
         Class<?> c1 = CL1.loadClass("class_loader.Singleton");
         System.out.println("Loaded class «Singleton» via the «CL1» class loader");
         Field flag = c1.getDeclaredField("runOnce");
         flag.setAccessible(true);
         System.out.println("Let's instatiate «Singleton@CL1»\n### runOnce :- "+flag.get(null));
+
         Object x = c1.getDeclaredConstructor().newInstance();
-        System.out.println("### runOnce :- "+flag.get(null));
+            System.out.println("### runOnce :- "+flag.get(null));
+
         try {
             System.out.println("Let's re-instantiate «Singleton@CL1»\n### runOnce :- "+flag.get(null));
             Object y = c1.getDeclaredConstructor().newInstance();
@@ -72,7 +76,15 @@ class SingletonViolationTest {
         SimpleClassLoader CL2 = new SimpleClassLoader("testclasses");
         System.out.println("Loaded class «Singleton» via the «CL2» class loader");
         Class<?> c2 = CL2.loadClass("class_loader.Singleton");
-        System.out.println(c1.getClassLoader() == c2.getClassLoader()); // What??? True??? Same package???
+
+        System.out.println(c1.getClassLoader().toString() + "\n" +
+        c2.getClassLoader().toString());
+        System.out.println("Same class loader: " + (c1.getClassLoader() == 
+        c2.getClassLoader())); // What??? True??? Same package???
+        System.out.println("Same class loader?: "+ CL1.equals(CL2));
+        System.out.println("Same class loader2? :"+c1.getClassLoader().equals(c2.getClassLoader()));
+        System.out.println("NONE'POSSIBILE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"+s.getClass().getClassLoader().equals(c2.getClassLoader()));        
+        
         Field flag2 = c2.getDeclaredField("runOnce");
         flag2.setAccessible(true);
         System.out.println("Let's instatiate «Singleton@CL2»\n### runOnce :- "+flag2.get(null));
