111c4d404a3e13f3fbb135aef9afcdf9a2e16334
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2022-11-14 14:34:42 +0100

    Update `wordle`
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- advanced-programming/practice/exams/2022-01-../wordle.py
+++ advanced-programming/practice/exams/2022-01-../wordle.py
@@ -1,22 +1,31 @@
 def position(word, secret):
-    yellow_letter = list(set(word).intersection(set(secret)))
-    green_letter = [ch if ch == secret[i] else ' ' for i, ch in enumerate(word)]
+    yellow_letter = set(word).intersection(set(secret))
+    green_letter = [ch if ch == secret[i] else " " for i, ch in enumerate(word)]
     black_letter = set(word) - set(secret)
-    return yellow_letter, green_letter, black_letter
+    return list(yellow_letter), green_letter, list(black_letter)
 
 def matching(list_words, word, secret):
     yellow, green, black = position(word, secret)
-    green = list(filter(lambda x: all([green[i] == x[i] or green[i] == ' ' for i in range(len(green))]), list_words))
-    print(green)
-    print(black)
-    black_green = set(green).difference(list(filter(lambda x: set(x).intersection(set(black)), green)))
-    print(black_green)
+    # print(yellow, green, black)
+    green = list(filter(lambda x: all([green[i] == x[i] or green[i] == " " for i in range(len(green))]), list_words))
+    # print(green)
+    black_green = list(filter(lambda x: len(set(x).intersection(set(black))) == 0, green))
+    # print(black_green)
+    final = list(filter(lambda x: len(set(x).intersection(set(yellow))) != 0 , black_green)) if (len(yellow) > 0) else black_green
+    # print(final)
+    return final
 
-def main(file):
-    with open(file, encoding='utf-8') as f:
-        list = f.read().split("\n")
-    matching(list, "about", "admit")
 
-#position("foods", "seeds")
-main("words.txt") 
-        
+if name == "main":
+    with open("Esami\words.txt", encoding='utf-8') as f:
+        words = f.read().split("\n")
+
+        def rec(lista, i=1):
+            if len(lista) == 1 : return lista[0]
+            word = lista[0]
+            lista = matching(lista, lista[0], "zesty")
+            if word in lista: lista.remove(word)
+            return rec(lista, i+1)
+
+        print(rec(words))
+       
