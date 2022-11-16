67cea32445a3dbdc132a6a86aa0282a1287984b1
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2022-11-16 15:09:09 +0100

    Update `16.11.2022`
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- advanced-programming/practice/exams/2022-01-../dict.py
+++ advanced-programming/practice/exams/2022-01-../dict.py
@@ -1,14 +1,17 @@
 def _get(k): return None
-def show(): return ""
-def get(k): return None if _get(k) == -1 else _get(k)
-    
+def _show(): return ""
+def get(k): return None if _get(k) == 'DoesNotExist' else _get(k)
+def show():
+    memo = []
+    return ' '.join([((element[0] not in memo and (memo.append(element[0]) or True) and element) or '') for element in _show().split()])
+
 def add(key, value):
     old_get = globals()["_get"]
     globals()["_get"] = lambda k: key==k and value or old_get(k)
-    old_show = globals()["show"]
-    globals()["show"] = lambda : f"{key}:{value} "+ old_show() 
+    old_show = globals()["_show"]
+    globals()["_show"] = lambda : f"{key}:{value} " + old_show() 
 
-def remove(key, value=-1):
+def remove(key, value='DoesNotExist'):
     return add(key, value)
 
 add('1', '1')
@@ -18,8 +21,12 @@ add('4', '4')
 print(show())
 print(get('2'))
 remove('2')
+print(show())
 print(get('2'))
-
+add('2', '2')
+print(show())
+print(get('2'))
+print(show())
 
 
 # def find(k):
