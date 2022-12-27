d0eef45ff82d0673b6ca5cf0e66d82ce167f4acb
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2022-12-27 09:49:53 +0100

    Update `parallel and distributed algorithm`
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- advanced-programming-techniques/practice/aspectj/lesson_aspectj/01/Main.java
+++ advanced-programming-techniques/practice/aspectj/lesson_aspectj/01/Main.java
@@ -1,10 +1,10 @@
 class Main {
  public static void main(String[] args) {
-  Line l = new Line();
-  Point p = new Point();
-  p.setX(10);
-  p.setY(1024);
-  l.setP1(p);
-  l.setP2(p);
+	Line l = new Line();
+	Point p = new Point();
+	p.setX(10);
+	p.setY(1024);
+	l.setP1(p);
+	l.setP2(p);
  }
 }
