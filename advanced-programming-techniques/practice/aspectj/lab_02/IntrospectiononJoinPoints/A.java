c2209ba71509f67ff9a684f19499320ccf21a961
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2023-01-05 12:45:56 +0100

    Add `SubIntrospection` test
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- advanced-programming-techniques/practice/aspectj/lab_02/IntrospectiononJoinPoints/A.java
+++ advanced-programming-techniques/practice/aspectj/lab_02/IntrospectiononJoinPoints/A.java
@@ -1,6 +1,10 @@
 public class A {
+    
+    public int i_a = 0;
 
-    public A() {}
+    public A(int i_a) {
+        this.i_a = i_a;
+		}
 
     public void test() {
         System.out.println("A::a");
