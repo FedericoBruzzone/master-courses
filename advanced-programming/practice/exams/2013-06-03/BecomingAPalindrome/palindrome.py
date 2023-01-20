62bf56962890b5ec15b1b6711d6de0f8571592e5
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2023-01-20 20:12:36 +0100

    Update `PA`
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- advanced-programming/practice/exams/2013-06-03/BecomingAPalindrome/palindrome.py
+++ advanced-programming/practice/exams/2013-06-03/BecomingAPalindrome/palindrome.py
@@ -1,9 +1,26 @@
 def palin(s: str):
   def inner(s: str, i: int):
-    for j in range(j, len(s)):
-      print("hello")
-  
-  for i in range(s):
-    left_to_right += inner(s, i)
-    right_to_left += inner(s[::-1], i)
+    res = []
+    stack = [s[j] for j in range(i)]
+    print("Stack: ", stack)
+    first = True
+    for j in range(i, len(s)):
+      stack = stack[:-1] if first and len(stack) > 1 and stack[-1] == stack[-2] else stack
+      if len(stack) == 0: 
+        res += list(s[j:]) 
+        break
+      while len(stack) > 0 and stack[-1] != s[j] and not first:
+        res += list(stack[-1])
+        stack = stack[:-1]
+      first = False
+      stack = stack[:-1]
+    return res + stack
+ 
+  tmp = []
+  print("Word: ", s)
+  for i in range(1, len(s)):
+    tmp.append(inner(s, i))
+    tmp.append(inner(s[::-1], i))
+  res = min(tmp, key=len) 
+  print(res, tmp)
   return ""
